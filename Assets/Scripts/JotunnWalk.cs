3a7e632aefee6a8cef618ad6fbb932413b4123c9
ArnauGonzalez-upf <arnau.gonzalez02@estudiant.upf.edu>
2022-02-10 02:07:03 +0100

    mucha cosa

---
+++ Assets/Scripts/JotunnWalk.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JotunnWalk : MonoBehaviour
+{
+
+    // Variables privadas
+    private Vector3 waypointActual;
+    private int wpElegido;
+
+    // @ Variables Publicas
+    public float velocidad;
+
+    // @ Arrays
+    private ArrayList listaWaypoints = new ArrayList();
+
+    // @ Comportamiento
+    private bool walk = true;
+
+    void Start()
+    {
+        // @ Agregamos las posiciones de los waypoints a la lista
+        GameObject[] waypoints = GameObject.FindGameObjectsWithTag("waypoint");
+        foreach (GameObject waypoint in waypoints)
+        {
+            listaWaypoints.Add(waypoint.transform.position);
+        }
+
+        // @ Definimos el primer waypoint al cual dirigirse
+        waypointActual = (Vector3)listaWaypoints[0];
+    }
+
+
+    void Update()
+    {
+        comprobarDistancia();
+        if (walk)
+            Walk();
+        else
+        {
+            StartCoroutine(Wait());
+        }
+
+    }
+
+    void Walk()
+    {
+        //GetComponent<Animation>().CrossFade("walk");
+        moveNPCTowards(waypointActual);
+    }
+
+    IEnumerator Wait()
+    {
+        //GetComponent<Animation>().CrossFade("idle");
+        yield return new WaitForSeconds(5.0f);
+        walk = true;
+    }
+
+
+    void comprobarDistancia()
+    {
+        // Elegimos un waypoint nuevo cuando ya se acerco lo suficiente al actual
+        if (Vector3.Distance(transform.position, waypointActual) < 10.0f)
+            selectNewWaypoint();
+    }
+
+    void selectNewWaypoint()
+    {
+        if (++wpElegido == listaWaypoints.Count) wpElegido = 0;
+        waypointActual = (Vector3)listaWaypoints[wpElegido];
+
+        walk = false;
+    }
+
+    void moveNPCTowards(Vector3 waypointActual)
+    {
+
+        // Direccion: definimos el vector direccion hacia el cual nos vamos a mover (restamos posicion objetivo - posicion nuestra)
+        Vector3 direccion = waypointActual - transform.position;
+        // Movimiento:_Son "nuevas posiciones" cada vez mas cercanas al objetivo.
+        // normalizar el vector es como obtener en lugar de toda la ruta, en que direccion tiene que dar "cada pequeño paso"
+        // al multiplicar por la velocidad, determinamos que tan "largo" es ese paso.
+        // usamos time.deltatime para que tenga un movimiento smooth respecto de los frames.
+
+        Vector3 movimiento = direccion.normalized * velocidad * Time.deltaTime;
+
+        transform.LookAt(waypointActual);
+        transform.position = transform.position + movimiento;
+
+    }
+
+
+
+}
