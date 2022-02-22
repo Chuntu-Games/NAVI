6b826beb352782bfd4c16bc871bf301ed72d9f21
ArnauGonzalez-upf <arnau.gonzalez02@estudiant.upf.edu>
2022-02-22 12:49:55 +0100

    hud finished

--- Assets/Scripts/ItemObject.cs
+++ Assets/Scripts/ItemObject.cs
@@ -8,6 +8,8 @@ public class ItemObject : MonoBehaviour
     public bool pickUp;
     public bool weapon;
     public GameObject pickObject;
+    public GameObject flashlight;
+    public GameObject weaponObj;
 
     public void OnHandlePickupItem()
     {
@@ -24,8 +26,11 @@ public class ItemObject : MonoBehaviour
             Destroy(gameObject);
             EquippedObjects.flashlightEquipped = true;
             EquippedObjects.flashlightUse = true;
-            EquippedObjects.weaponUse = false;
-
+            if (EquippedObjects.weaponUse)
+            {
+                weaponObj.SetActive(false);
+                EquippedObjects.weaponUse = false;
+            }
         }
         else if (pickUp && weapon)
         {
@@ -33,7 +38,11 @@ public class ItemObject : MonoBehaviour
             Destroy(gameObject);
             EquippedObjects.weaponEquipped = true;
             EquippedObjects.weaponUse = true;
-            EquippedObjects.flashlightUse = false;
+            if (EquippedObjects.flashlightUse)
+            {
+                flashlight.SetActive(false);
+                EquippedObjects.flashlightUse = false;
+            }
         }
         else
             OnHandlePickupItem();
